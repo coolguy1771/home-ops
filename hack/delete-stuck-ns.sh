43ca8c446650609cc706fe7ef654be10a8fef74a
Tyler Witlin <45468117+coolguy1771@users.noreply.github.com>
2022-02-15 15:39:22 -0700

    fix(hack): Use double quote to prevent globbing and word splitting

--- hack/delete-stuck-ns.sh
+++ hack/delete-stuck-ns.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 function delete_namespace () {
-    echo "Deleting namespace "$1""
+    echo "Deleting namespace $1"
     kubectl get namespace "$1" -o json > tmp.json
     sed -i 's/"kubernetes"//g' tmp.json
     kubectl replace --raw "/api/v1/namespaces/$1/finalize" -f ./tmp.json
@@ -12,5 +12,5 @@ TERMINATING_NS=$(kubectl get ns | awk '$2=="Terminating" {print $1}')
 
 for ns in $TERMINATING_NS
 do
-    delete_namespace $ns
+    delete_namespace "$ns"
 done
