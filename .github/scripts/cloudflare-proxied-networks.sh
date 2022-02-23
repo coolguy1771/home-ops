543fd883c5a52711012554e5ae79a5751cbfadb5
Tyler Witlin <45468117+coolguy1771@users.noreply.github.com>
2022-02-22 18:17:58 -0700

    fix(ci): edit cloudflare proxy script

--- .github/scripts/cloudflare-proxied-networks.sh
+++ .github/scripts/cloudflare-proxied-networks.sh
@@ -1,23 +1,26 @@
 #!/usr/bin/env bash
 
+# Get all local networks
+# ipv4_rfc1918='[ "10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16" ]'
+
 # Get all cloudflare ipv4 ranges in an array
-ipv4="$(curl -s https://www.cloudflare.com/ips-v4 | jq --raw-input --slurp 'split("\n")')"
-if [[ -z "${ipv4}" ]]; then
+ipv4_cloudflare="$(curl -sL https://www.cloudflare.com/ips-v4 | jq --raw-input --slurp 'split("\n")')"
+if [[ -z "${ipv4_cloudflare}" ]]; then
     exit 1
 fi
 
 # Get all cloudflare ipv6 ranges in an array
-ipv6="$(curl -s https://www.cloudflare.com/ips-v6 | jq --raw-input --slurp 'split("\n")')"
-if [[ -z "${ipv6}" ]]; then
+ipv6_cloudflare="$(curl -sL https://www.cloudflare.com/ips-v6 | jq --raw-input --slurp 'split("\n")')"
+if [[ -z "${ipv6_cloudflare}" ]]; then
     exit 1
 fi
 
-# Merge both cloudflare ipv4 and ipv6 ranges into one array
-ipv4ipv6=$(jq \
-    --argjson arr1 "$ipv4" \
-    --argjson arr2 "$ipv6" \
-    -n '$arr1 + $arr2 | sort_by(.)' \
+# Merge rfc1918 ipv4, cloudflare ipv4, and cloudflare ipv6 ranges into one array
+combined=$(jq \
+    --argjson ipv4_cloudflare "${ipv4_cloudflare}" \
+    --argjson ipv6_cloudflare "${ipv6_cloudflare}" \
+    -n '$ipv4_cloudflare + $ipv6_cloudflare' \
 )
 
 # Output array as a string with \, as delimiter
-echo "${ipv4ipv6}" | jq --raw-output '. | join("\\,")'
+echo "${combined}" | jq --raw-output '. | join("\\,")'
