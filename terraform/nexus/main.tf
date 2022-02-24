5a478b0fc93f5e910537b1d1a42e6160d44abae0
Tyler Witlin <45468117+coolguy1771@users.noreply.github.com>
2022-02-24 16:29:50 -0700

    fix(terraform): update nexus configuration
    Signed-off-by: Tyler Witlin <45468117+coolguy1771@users.noreply.github.com>

--- terraform/nexus/main.tf
+++ terraform/nexus/main.tf
@@ -30,18 +30,50 @@ provider "nexus" {
   password = data.sops_file.nexus_secrets.data["nexus_password"]
 }
 
-resource "nexus_repository_docker_hosted" "docker_local" {
-  name   = "docker-local"
-  online = true
 
-  component {
-    proprietary_components = false
-  }
+resource "nexus_repository_docker_proxy" "ghcr_io_mirror" {
+  name   = "ghcr-io-mirror"
+  online = true
 
   docker {
     force_basic_auth = false
-    http_port        = 8082
+    http_port        = 0
     https_port       = 0
+    v1_enabled       = true
+  }
+
+  docker_proxy {
+    index_type = "REGISTRY"
+  }
+
+  http_client {
+    auto_block = true
+    blocked    = false
+  }
+
+  negative_cache {
+    enabled = false
+    ttl     = 1440
+  }
+
+  proxy {
+    content_max_age  = 1440
+    metadata_max_age = 1440
+    remote_url       = "https://ghcr.io"
+
+  }
+
+  storage {
+    blob_store_name                = "default"
+    strict_content_type_validation = true
+  }
+}
+resource "nexus_repository_docker_hosted" "internal" {
+  name   = "internal"
+  online = "true"
+
+  docker {
+    force_basic_auth = false
     v1_enabled       = false
   }
 
@@ -55,16 +87,26 @@ resource "nexus_repository_docker_hosted" "docker_local" {
 resource "nexus_repository_docker_proxy" "docker_io_mirror" {
   name   = "docker-io-mirror"
   online = true
-
   docker {
     force_basic_auth = false
-    http_port        = 8083
+    http_port        = 0
     https_port       = 0
-    v1_enabled       = false
+    v1_enabled       = true
   }
 
   docker_proxy {
     index_type = "HUB"
+  }
+
+  storage {
+    blob_store_name                = "default"
+    strict_content_type_validation = true
+  }
+
+  proxy {
+    remote_url       = "https://registry-1.docker.io"
+    content_max_age  = 1440
+    metadata_max_age = 1440
   }
 
   http_client {
@@ -73,26 +115,40 @@ resource "nexus_repository_docker_proxy" "docker_io_mirror" {
   }
 
   negative_cache {
-    enabled = true
+    enabled = false
     ttl     = 1440
   }
 
-  proxy {
-    content_max_age  = 1440
-    metadata_max_age = 1440
-    remote_url       = "https://registry-1.docker.io"
-  }
-
-  storage {
-    blob_store_name                = "default"
-    strict_content_type_validation = true
-  }
 }
-
 resource "nexus_security_realms" "active_realms" {
   active = [
     "NexusAuthenticatingRealm",
     "NexusAuthorizingRealm",
     "DockerToken"
   ]
+}
+
+resource "nexus_repository_docker_group" "group" {
+  name   = "docker-group"
+  online = true
+
+  docker {
+    force_basic_auth = false
+    http_port        = 8083
+    https_port       = 0
+    v1_enabled       = false
+  }
+
+  group {
+    member_names = [
+      nexus_repository_docker_hosted.internal.name,
+      nexus_repository_docker_proxy.docker_io_mirror.name,
+      nexus_repository_docker_proxy.ghcr_io_mirror.name
+    ]
+  }
+
+  storage {
+    blob_store_name                = "default"
+    strict_content_type_validation = true
+  }
 }
