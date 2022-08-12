a7b348dfd7e6f5523e464020ea81f7f0d90f7382
Tyler Witlin <tyler@codezero.io>
2022-08-11 23:20:28 -0400

    fix(cluster): update script to have outputs
    Signed-off-by: Tyler Witlin <tyler@codezero.io>

--- cluster/apps/networking/opnsense/backup/opnsense-backup.sh
+++ cluster/apps/networking/opnsense/backup/opnsense-backup.sh
@@ -4,19 +4,25 @@ set -o nounset
 set -o errexit
 
 FILE_DATE=$(date "+%Y%m%d-%H%M%S")
-
-curl --silent -k --user "${OPNSENSE_KEY}":"${OPNSENSE_SECRET}" https://"${OPNSENSE_HOST}"/api/backup/backup/download --output "opnsense-backup-${FILE_DATE}.xml"
-FILE="opnsense-backup-${FILE_DATE}.xml"
-
 RESOURCE="/${S3_BUCKET}/${FILE}"
 CONTENT_TYPE="text/xml"
 DATE=$(date -R)
 _signature="PUT\n\n${CONTENT_TYPE}\n${DATE}\n${RESOURCE}"
 SIGNATURE=$(echo -en "${_signature}" | openssl sha1 -hmac "${S3_SECRET_KEY}" -binary | base64)
 
-curl --silent -X PUT -T "${FILE}" \
+echo "Attempting to download Opnsense config file..."
+curl --silent -k --user "${OPNSENSE_KEY}":"${OPNSENSE_SECRET}" https://"${OPNSENSE_HOST}"/api/backup/backup/download --output "opnsense-backup-${FILE_DATE}.xml"
+FILE="opnsense-backup-${FILE_DATE}.xml"
+echo "Successfully downloaded Opnsense config file to ${FILE}..."
+
+echo "Uploading backup to ${S3_BUCKET} on ${S3_HOST}..."
+STATUS=$(curl --silent -X PUT -T "${FILE}" \
         -H "Host: ${S3_ENDPOINT}" \
         -H "Date: ${DATE}" \
         -H "Content-Type: ${CONTENT_TYPE}" \
         -H "Authorization: AWS ${S3_ACCESS_KEY}:${SIGNATURE}" \
-        https://"${S3_ENDPOINT}""${RESOURCE}"
+        https://"${S3_ENDPOINT}""${RESOURCE}")
+
+echo "${STATUS}" || echo "Upload Completed Successfully"
+
+echo "You have successfully backed up your Opnsense config file to ${S3_BUCKET} on ${S3_HOST}."
