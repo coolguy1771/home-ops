e1a9066a4cd43c3dfc81b8629f513ace8a05981e
Tyler Witlin <tyler@codezero.io>
2022-12-06 10:23:28 -0700

    fix(cluster): update opnsense backup
    Signed-off-by: Tyler Witlin <tyler@codezero.io>

--- cluster/apps/networking/opnsense/backup/opnsense-backup.sh
+++ cluster/apps/networking/opnsense/backup/opnsense-backup.sh
@@ -4,22 +4,28 @@ set -o nounset
 set -o errexit
 
 config_filename="$(date "+%Y%m%d-%H%M%S").xml"
+
+http_host=${S3_URL#*//}
+http_host=${http_host%:*}
 http_request_date=$(date -R)
-http_filepath="${S3_BUCKET}/${config_filename}"
-sig="PUT\n\ntext/xml\n${http_request_date}\n/${http_filepath}"
-http_signature=$(echo -en "${sig}" | openssl sha1 -hmac "${AWS_SECRET_ACCESS_KEY}" -binary | base64)
+http_filepath="opnsense-backup/${config_filename}"
+http_signature=$(
+    printf "PUT\n\ntext/xml\n%s\n/%s" "${http_request_date}" "${http_filepath}" \
+        | openssl sha1 -hmac "${AWS_SECRET_ACCESS_KEY}" -binary \
+        | base64
+)
 
 echo "Download Opnsense config file ..."
 curl -fsSLk \
-        --user "${OPNSENSE_KEY}:${OPNSENSE_SECRET}" \
-        --output "${config_filename}" \
-        "https://${OPNSENSE_HOST}/api/backup/backup/download"
+    --user "${OPNSENSE_KEY}:${OPNSENSE_SECRET}" \
+    --output "/tmp/${config_filename}" \
+    "${OPNSENSE_URL}/api/backup/backup/download"
 
 echo "Upload backup to s3 bucket ..."
 curl -fsSL \
-        -X PUT -T "${config_filename}" \
-        -H "Host: ${S3_ENDPOINT}" \
-        -H "Date: ${http_request_date}" \
-        -H "Content-Type: text/xml" \
-        -H "Authorization: AWS ${AWS_ACCESS_KEY_ID}:${http_signature}" \
-        "https://${S3_ENDPOINT}/${http_filepath}"
+    -X PUT -T "/tmp/${config_filename}" \
+    -H "Host: ${http_host}" \
+    -H "Date: ${http_request_date}" \
+    -H "Content-Type: text/xml" \
+    -H "Authorization: AWS ${AWS_ACCESS_KEY_ID}:${http_signature}" \
+    "${S3_URL}/${http_filepath}"
