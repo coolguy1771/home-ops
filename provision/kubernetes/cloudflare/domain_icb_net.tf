26525a54fe3715f04db1fbe4e2c7dab47d4ee8eb
Tyler Witlin <twitlin@witl.xyz>
2023-05-06 10:20:29 -0400

    fix(terraform): update dns records
    Signed-off-by: Tyler Witlin <twitlin@witl.xyz>

--- provision/kubernetes/cloudflare/domain_icb_net.tf
+++ provision/kubernetes/cloudflare/domain_icb_net.tf
@@ -1,12 +1,10 @@
-data "cloudflare_zones" "icb_domain" {
-  filter {
+data "cloudflare_zone" "icb_domain" {
     name = "icbplays.net"
-  }
 }
 
 resource "cloudflare_record" "arma" {
   name    = "arma"
-  zone_id = lookup(data.cloudflare_zones.icb_domain.zones[0], "id")
+  zone_id = data.cloudflare_zone.icb_domain.id
   value   = chomp(data.http.ipv4.response_body)
   proxied = false
   type    = "A"
@@ -15,7 +13,7 @@ resource "cloudflare_record" "arma" {
 
 resource "cloudflare_record" "valheim" {
   name    = "valheim"
-  zone_id = lookup(data.cloudflare_zones.icb_domain.zones[0], "id")
+  zone_id = data.cloudflare_zone.icb_domain.id
   value   = chomp(data.http.ipv4.response_body)
   proxied = false
   type    = "A"
@@ -24,7 +22,7 @@ resource "cloudflare_record" "valheim" {
 
 resource "cloudflare_record" "icb_domain_www" {
   name    = "www"
-  zone_id = lookup(data.cloudflare_zones.icb_domain.zones[0], "id")
+  zone_id = data.cloudflare_zone.icb_domain.id
   value   = "ipv4.icbplays.net"
   proxied = true
   type    = "CNAME"
@@ -33,7 +31,7 @@ resource "cloudflare_record" "icb_domain_www" {
 
 resource "cloudflare_record" "icb_domain_apex" {
   name    = "ipv4"
-  zone_id = lookup(data.cloudflare_zones.icb_domain.zones[0], "id")
+  zone_id = data.cloudflare_zone.icb_domain.id
   value   = chomp(data.http.ipv4.response_body)
   proxied = false
   type    = "A"
@@ -42,7 +40,7 @@ resource "cloudflare_record" "icb_domain_apex" {
 
 resource "cloudflare_record" "icb_domain_cf_mx1" {
   name     = "icbplays.net"
-  zone_id  = lookup(data.cloudflare_zones.icb_domain.zones[0], "id")
+  zone_id  = data.cloudflare_zone.icb_domain.id
   value    = "amir.mx.cloudflare.net"
   proxied  = false
   type     = "MX"
@@ -52,7 +50,7 @@ resource "cloudflare_record" "icb_domain_cf_mx1" {
 
 resource "cloudflare_record" "icb_domain_cf_mx2" {
   name     = "icbplays.net"
-  zone_id  = lookup(data.cloudflare_zones.icb_domain.zones[0], "id")
+  zone_id  = data.cloudflare_zone.icb_domain.id
   value    = "linda.mx.cloudflare.net"
   proxied  = false
   type     = "MX"
@@ -61,7 +59,7 @@ resource "cloudflare_record" "icb_domain_cf_mx2" {
 }
 resource "cloudflare_record" "icb_domain_cf_mx3" {
   name     = "icbplays.net"
-  zone_id  = lookup(data.cloudflare_zones.icb_domain.zones[0], "id")
+  zone_id  = data.cloudflare_zone.icb_domain.id
   value    = "issac.mx.cloudflare.net"
   proxied  = false
   type     = "MX"
@@ -72,7 +70,7 @@ resource "cloudflare_record" "icb_domain_cf_mx3" {
 
 resource "cloudflare_record" "icb_domain_cf_dmarc" {
   name    = "_dmarc"
-  zone_id = lookup(data.cloudflare_zones.icb_domain.zones[0], "id")
+  zone_id = data.cloudflare_zone.icb_domain.id
   value   = "v=DMARC1; p=none; rua=mailto:dmac@icbplays.net"
   proxied = false
   type    = "TXT"
@@ -81,7 +79,7 @@ resource "cloudflare_record" "icb_domain_cf_dmarc" {
 
 resource "cloudflare_record" "icb_domain_cf_spf" {
   name    = "icbplays.net"
-  zone_id = lookup(data.cloudflare_zones.icb_domain.zones[0], "id")
+  zone_id = data.cloudflare_zone.icb_domain.id
   value   = "v=spf1 include:_spf.mx.cloudflare.net ~all"
   proxied = false
   type    = "TXT"
