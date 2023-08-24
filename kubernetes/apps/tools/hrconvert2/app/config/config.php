9bfaa7f6e00196191da7dda01e6562023015055d
Tyler Witlin <twitlin@witl.xyz>
2023-08-24 08:57:34 -0400

    fix(cluster): deploy with config
    Signed-off-by: Tyler Witlin <twitlin@witl.xyz>

---
+++ kubernetes/apps/tools/hrconvert2/app/config/config.php
@@ -0,0 +1,58 @@
+<?php
+// SET RANDOM STRINGS HERE DONT USE DEFAULTS
+$Accept_GPLv3_OpenSource_License = '1';
+$Salts1 = 'PutSomethingRandomHere';
+$Salts2 = 'PutSomethingElseRandomHere';
+$Salts3 = 'AndSomethingHere';
+$Salts4 = 'TheLongerTheBetter';
+$Salts5 = 'MoreRandomStuff';
+$Salts6 = 'LastOne';
+
+//Put the URL used to access HRConvert here
+$URL = 'localhost:8085';
+
+//Set to true to enable virus scanning during all file operations
+$VirusScan = TRUE;
+
+//Set application browser application name and title
+$ApplicationName = 'HRConvert2';
+$ApplicationTitle = 'Convert Anything!';
+
+$SupportedLanguages = array('en', 'fr', 'es', 'zh', 'hi', 'ar', 'ru', 'uk', 'bn', 'de', 'ko', 'it', 'pt');
+//Default language
+$DefaultLanguage = 'en';
+$AllowUserSelectableLanguage = TRUE;
+
+//Set to FALSE to disable users creating share links
+$AllowUserShare = TRUE;
+
+//Button colour - RED,GREEN,BLUE,GREY
+$ButtonStyle = 'BLUE';
+
+$SpinnerStyle = 6;
+$SpinnerColor = $ButtonStyle;
+$ShowGUI = TRUE;
+$ShowFinePrint = FALSE;
+
+
+
+
+$DeleteThreshold = 30;
+$Verbose = TRUE;
+$AllowUserVirusScan = TRUE;
+$ScanCoreMemoryLimit = 268435456;
+$ScanCoreChunkSize = 134217728;
+$ScanCoreDebug = FALSE;
+$ScanCoreVerbose = TRUE;
+$InstLoc = '/var/www/html/HRProprietary/HRConvert2';
+$ServerRootDir = '/var/www/html';
+$ConvertLoc = '/home/converter';
+$LogDir = '/var/www/html/HRProprietary/HRConvert2/Logs';
+$Delete_Threshold = '30';
+$MaxLogSize = '1048576';
+
+
+$Font = 'Verdana';
+
+$TOSURL = 'https://www.honestrepair.net/index.php/terms-of-service/';
+$PPURL = 'https://www.honestrepair.net/index.php/privacy-policy/';
